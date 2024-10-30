117e6f7317ca5fcfa06de9fc0afb9b68fcba9225
Annete Montserrat Cedillo Mariscal <141747399+annetecm@users.noreply.github.com>
2024-10-30 14:42:20 +0000

    feat: add hello world

---
+++ hola.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){ //argc te dice cuantos elementos hay en la cadena
+
+    for (int i=1;i<argc;i++){
+    printf("hola mundo %s\n", argv[i]); //el primer argumento siempre es que nombre del programa
+    }
+    return 0;
+}
